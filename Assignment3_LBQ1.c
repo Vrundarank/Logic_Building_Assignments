1b4e6df748101a0317f20a7ab9413678c9042505
Vrundarank <140796087+Vrundarank@users.noreply.github.com>
2025-10-24 23:37:45 +0530

    Add files via upload

---
+++ Assignment3_LBQ1.c
@@ -0,0 +1,15 @@
+//write a program which accept one number from user and print that number of even numbers on screen
+
+#include<stdio.h>
+
+void PrintEven(int iNo)
+{
+    int iCnt = 0;
+    if(iNo <= 0)
+    {
+        return ;
+    }
+    //Logic
+    
+    
+}
